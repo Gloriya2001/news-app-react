7aec6c3f415ea7751d97ff270d85303e3f699799
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-08 14:17:02 +0530

    view semi dynamic done

--- src/components/ViewNews.jsx
+++ src/components/ViewNews.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ViewNews = () => {
+
+    const [news, changeNews] = useState()
+
+
+
+
+
     return (
         <div>
 
@@ -10,19 +17,32 @@ const ViewNews = () => {
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                         <div className="row">
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                <div class="card">
-                                    <img src="..." class="card-img-top" alt="..."></img>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Card title</h5>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                            {news.articles.map(
+                                (value, index) => {
+
+                                    return <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                        <div class="card">
+                                            <img src={value.urlToImage} class="card-img-top" alt="..."></img>
+                                            <div class="card-body">
+                                                <h5 class="card-title">{value.author}</h5>
+                                                <p class="card-text">{value.title}</p>
+                                               
+                                                <a href="#" class="btn btn-primary">Read More</a>
+                                            </div>
                                         </div>
-                                </div>
 
 
-                            </div>
+                                    </div>
+
+                                }
+                            )}
+
+
+                            
+
                         </div>
 
 
